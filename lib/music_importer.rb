e8344f0841563fbdb0ee781a9751ce7884be1767
Travis Gandy <travis_gandy@yahoo.com>
2020-05-25 00:41:08 +0000

    Automatically backed up by Learn

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -8,6 +8,7 @@ class MusicImporter
   def files
       Dir["#{path}/**/*.mp3"].map{|file| file.slice(path.size + 1..file.size)}
   end 
+  
   def self.import 
       files.each{|file| Song.create_from_filename(file)}
   end 

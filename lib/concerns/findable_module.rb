5bc19264582bc4af9eadcf1babbb0657726cc30f
Travis Gandy <travis_gandy@yahoo.com>
2020-05-25 00:14:28 +0000

    Automatically backed up by Learn

--- lib/concerns/findable_module.rb
+++ lib/concerns/findable_module.rb
@@ -4,6 +4,8 @@ module Concerns::Findable
         self.all.find{|genre_or_artist| genre_or_artist.name == search}
     end 
     
-    
+    def self.find_or_create_by_name(search)
+      self.find_by_name(search) ? (search = self.find_by_name(search)) : self.create(search)
+    end 
 
 end

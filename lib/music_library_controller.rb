700d12f26e13fd45964cf53d26033846cced76a1
Travis Gandy <travis_gandy@yahoo.com>
2020-05-25 01:08:25 +0000

    Automatically backed up by Learn

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -38,6 +38,7 @@ class MusicLibraryController
   def list_songs
     Song.all.sort{ |a, b| a.name <=> b.name }.each.with_index(1){|s, i|
       puts "#{i}. #{s.artist.name} - #{s.name} - #{s.genre.name}"}
-    end
+  end
+  
   
 end 

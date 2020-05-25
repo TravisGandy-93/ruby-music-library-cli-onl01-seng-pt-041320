e295e471df777bdb7a4acaece8237385959c3445
Travis Gandy <travis_gandy@yahoo.com>
2020-05-25 00:29:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -56,6 +56,14 @@ class Song
       self.find_by_name(search) ? (search = self.find_by_name(search)) : self.create(search)
   end 
 
-  def 
+  def self.new_by_filename(filename)
+    artist_name = filename.split(" - ")[0] 
+     song_name = filename.split(" - ")[1] 
+     song = Song.new(song_name) 
+     song.artist =  Artist.find_or_create_by_name(artist_name)
+     song.artist.add_song(self) 
+     song  
+  end 
+
   
 end 

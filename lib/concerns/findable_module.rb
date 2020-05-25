285e476db521339d7179e6005a2a5656011b2ada
Travis Gandy <travis_gandy@yahoo.com>
2020-05-25 00:13:41 +0000

    Automatically backed up by Learn

--- lib/concerns/findable_module.rb
+++ lib/concerns/findable_module.rb
@@ -1,3 +1,9 @@
 module Concerns::Findable
   # Module code here
+    def self.find_by_name(search)
+        self.all.find{|genre_or_artist| genre_or_artist.name == search}
+    end 
+    
+    
+
 end
